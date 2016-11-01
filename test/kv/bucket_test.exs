1162dc5c99a5fed75523fb8635fd2d4ddbeb15e9
Ryan Flach <ryanflach@users.noreply.github.com>
2016-11-01 17:33:48 -0600

    Add initial bucket test and functionality - Adds get and put functionality to the bucket

---
+++ test/kv/bucket_test.exs
@@ -0,0 +1,12 @@
+defmodule KV.BucketTest do
+  # Able to use async due to no interaction with global values.
+  use ExUnit.Case, async: true
+
+  test "stores values by key" do
+    {:ok, bucket} = KV.Bucket.start_link
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+end
